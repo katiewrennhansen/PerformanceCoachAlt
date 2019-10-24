b9720be205eaab74d182b449cff99399d7d6b59c
Katie Hansen <khansen0415@gmail.com>
2019-10-24 14:49:23 -0400

    add transparent logo, remove extra images

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const controls = document.querySelectorAll('.image-controls');
 const auto = true;
 const intervalTime = 5000;
 let slideInterval;
-const contactForm = document.getElementById('contact-form');
 
 
 //FUNCTIONS
@@ -71,20 +70,11 @@ function autoSlide(){
 }
 
 
-// //handle contact form submission
-// function handleSubmit(e){
-//     e.preventDefault();
-//     const data = new FormData(contactForm)
-//     console.log(data)
-// }
-
-
 
 //EVENT LISTENERS
 toggleBtn.addEventListener('click', navToggle);
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
-contactForm.addEventListener('submit', handleSubmit)
 
 navLinks.forEach(link => {
     link.addEventListener('click', hideNav);
